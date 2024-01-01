22f2869f5e072aebcc6c0271acfd76923900abc4
Amit Singh <amitsskingh@gmail.com>
2024-01-01 12:01:56 +0530

    adding the work section

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,9 +1,9 @@
 const about = document.getElementById("about");
 const contacts = document.getElementById("contact");
-// const project = document.getElementById("work");
+const project = document.getElementById("work");
 const header = document.querySelector("header");
 const headerHeight = header.offsetHeight;
 
 about.style.scrollMarginTop = `${headerHeight}px`
 contacts.style.scrollMarginTop = `${headerHeight}px`
-// project.style.scrollMarginTop = `${headerHeight}px`
+project.style.scrollMarginTop = `${headerHeight}px`
